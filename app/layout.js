1a83c799113260dfd0fc4dcf2e3612417e52bf21
Gregor Arz <upstroke@webtutorials.de>
2026-09-24 22:10:26 +0200

    fix: import fomantic CSS parts directly in layout.js, remove from app.scss

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,17 @@
+import 'fomantic-ui-css/components/reset.css';
+import 'fomantic-ui-css/components/site.css';
+import 'fomantic-ui-css/components/container.css';
+import 'fomantic-ui-css/components/grid.css';
+import 'fomantic-ui-css/components/menu.css';
+import 'fomantic-ui-css/components/header.css';
+import 'fomantic-ui-css/components/card.css';
+import 'fomantic-ui-css/components/list.css';
+import 'fomantic-ui-css/components/button.css';
+import 'fomantic-ui-css/components/label.css';
+import 'fomantic-ui-css/components/icon.css';
+import 'fomantic-ui-css/components/image.css';
+import 'fomantic-ui-css/components/tab.css';
+import 'fomantic-ui-css/components/segment.css';
 import '../styles/app.scss';
 
 export const metadata = {
