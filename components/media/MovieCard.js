54592fc219809beed3b71c42b07f08455eaeb7cc
Gregor Arz <upstroke@webtutorials.de>
2026-09-24 20:23:48 +0200

    fix: update MovieCard and TvShowCard to use locale prop and correct routes

--- components/media/MovieCard.js
+++ components/media/MovieCard.js
@@ -1,39 +1,29 @@
-'use client';
-
 import Link from 'next/link';
-import { useLocale } from '@/lib/stores/localeStore';
 
 /**
- * @typedef {Object} MovieCardProps
- * @property {{ id: number, title: string, poster_path: string|null, vote_average: number, release_date: string }} movie
+ * @param {{ movie: { id: number, title: string, poster_path: string|null, vote_average: number, release_date: string }, locale: string }} props
  */
-
-/**
- * @param {MovieCardProps} props
- */
-export default function MovieCard({ movie }) {
-  const { locale } = useLocale();
-
+export function MovieCard({ movie, locale }) {
   const posterUrl = movie.poster_path
     ? `https://image.tmdb.org/t/p/w342${movie.poster_path}`
     : '/fallback-poster.png';
 
   return (
-    <li>
-      <Link href={`/movie/${movie.id}?locale=${locale}`} className="default-card">
-        <div className="ui card">
-          <div className="image">
-            <img src={posterUrl} alt={movie.title} />
-          </div>
-          <div className="content">
-            <div className="header">{movie.title}</div>
-            <div className="meta date">{movie.release_date}</div>
-          </div>
-          <div className="extra content">
-            <span className="rating-value">&#9733; {movie.vote_average?.toFixed(1)}</span>
-          </div>
+    <Link href={`/${locale}/movies/${movie.id}`} className="default-card">
+      <div className="ui card">
+        <div className="image">
+          <img src={posterUrl} alt={movie.title} />
         </div>
-      </Link>
-    </li>
+        <div className="content">
+          <div className="header">{movie.title}</div>
+          <div className="meta date">{movie.release_date}</div>
+        </div>
+        <div className="extra content">
+          <span className="rating-value">&#9733; {movie.vote_average?.toFixed(1)}</span>
+        </div>
+      </div>
+    </Link>
   );
 }
+
+export default MovieCard;
--- components/media/TvShowCard.js
+++ components/media/TvShowCard.js
@@ -1,39 +1,29 @@
-'use client';
-
 import Link from 'next/link';
-import { useLocale } from '@/lib/stores/localeStore';
 
 /**
- * @typedef {Object} TvShowCardProps
- * @property {{ id: number, name: string, poster_path: string|null, vote_average: number, first_air_date: string }} show
+ * @param {{ show: { id: number, name: string, poster_path: string|null, vote_average: number, first_air_date: string }, locale: string }} props
  */
-
-/**
- * @param {TvShowCardProps} props
- */
-export default function TvShowCard({ show }) {
-  const { locale } = useLocale();
-
+export function TvShowCard({ show, locale }) {
   const posterUrl = show.poster_path
     ? `https://image.tmdb.org/t/p/w342${show.poster_path}`
     : '/fallback-poster.png';
 
   return (
-    <li>
-      <Link href={`/tv/${show.id}?locale=${locale}`} className="default-card">
-        <div className="ui card">
-          <div className="image">
-            <img src={posterUrl} alt={show.name} />
-          </div>
-          <div className="content">
-            <div className="header">{show.name}</div>
-            <div className="meta date">{show.first_air_date}</div>
-          </div>
-          <div className="extra content">
-            <span className="rating-value">&#9733; {show.vote_average?.toFixed(1)}</span>
-          </div>
+    <Link href={`/${locale}/tv/${show.id}`} className="default-card">
+      <div className="ui card">
+        <div className="image">
+          <img src={posterUrl} alt={show.name} />
         </div>
-      </Link>
-    </li>
+        <div className="content">
+          <div className="header">{show.name}</div>
+          <div className="meta date">{show.first_air_date}</div>
+        </div>
+        <div className="extra content">
+          <span className="rating-value">&#9733; {show.vote_average?.toFixed(1)}</span>
+        </div>
+      </div>
+    </Link>
   );
 }
+
+export default TvShowCard;
