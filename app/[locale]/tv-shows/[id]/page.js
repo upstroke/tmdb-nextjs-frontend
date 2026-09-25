d0007ef36f76784ecd2a69c430dcdec0df4d5185
gregorarz <upstroke@webtutorials.de>
2026-09-25 12:03:48 +0200

    chore: ad more component styles

--- app/[locale]/tv-shows/[id]/page.js
+++ app/[locale]/tv-shows/[id]/page.js
@@ -188,8 +188,7 @@ export default async function TvShowDetailPage({ params }) {
 
         <section aria-labelledby="watch-providers-heading">
           <h3 id="watch-providers-heading" className="ui medium dividing header">
-            {labels.streamingProviders}
-            <small className="justwatch-attribution">{labels.streamingDataProvidedBy} <strong>&copy;JustWatch</strong></small>
+            {labels.streamingProviders} <small className="justwatch-attribution">{labels.streamingDataProvidedBy} <strong>&copy;JustWatch</strong></small>
           </h3>
           {providers?.providers?.length ? (
             <ul className="ui relaxed divided list providers-list">
